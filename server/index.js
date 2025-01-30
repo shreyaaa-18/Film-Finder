874dd12778c975de83aaf833a376cefc74f1c3bf
shreyaaa-18 <shreyaagarwal1811@gmail.com>
2025-01-30 23:09:16 +0530

    cors config updated

--- server/index.js
+++ server/index.js
@@ -3,7 +3,13 @@ import cors from 'cors'
 import authRouter from './routes/authRoutes.js'
 
 const app = express()
-app.use(cors())
+app.use(cors({
+    origin: [
+        'https://film-finderrr.netlify.app', // Production frontend
+        'http://localhost:3000' // For local development
+        ],
+      credentials: true // Enable if using cookies/tokens
+}))
 app.use(express.json())
 app.use('/auth', authRouter)
 
